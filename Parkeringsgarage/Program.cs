0c7b29e870899b9fe0a92031700b1058fdf77beb
Alexander Åberg <alexander_aberg1985@msn.com>
2024-11-01 10:13:19 +0100

    Initial Visual Studios commit

---
+++ Parkeringsgarage/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Parkeringsgarage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen till garaget!");
+        }
+    }
+}
