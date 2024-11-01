b3500c540b2a59606501d56dc60393c69a176c95
Alexander Åberg <alexander_aberg1985@msn.com>
2024-11-01 10:36:02 +0100

    Created the first menu level

---
+++ Parkeringsgarage/Incheckning.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parkeringsgarage
+{
+    public class Incheckning
+    {
+        public static void CheckIn()
+        {
+            Console.WriteLine("Välkommen till Smart Parking!");
+
+            while (true)
+            {
+                Console.WriteLine("Meny - Smart Parking");
+                Console.WriteLine("====");
+                Console.WriteLine("Tryck 1 för Parkeringsgäst");
+                Console.WriteLine("Tryck 2 för Parkeringsvakt");
+                Console.WriteLine("Tryck 3 för Parkeringschef");
+
+                ConsoleKeyInfo key = Console.ReadKey();
+                Console.Clear();
+
+                switch (key.KeyChar)
+                {
+                    case '1':
+                        ;
+                        break;
+                    case '2':
+                        ;
+                        break;
+                    case '3':
+                        ;
+                        break;
+                }
+            }
+        }
+    }
+}
