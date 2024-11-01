58b1d80e9aa18b15bb1e8564bc6092134817f359
Alexander Åberg <alexander_aberg1985@msn.com>
2024-11-01 13:03:00 +0100

    Created sub menu to be able to choose both kind of person and kind of vehicle with options

--- Parkeringsgarage/Incheckning.cs
+++ Parkeringsgarage/Incheckning.cs
@@ -26,16 +26,74 @@ namespace Parkeringsgarage
                 switch (key.KeyChar)
                 {
                     case '1':
-                        ;
+                        ParkingGuest();
                         break;
                     case '2':
-                        ;
+                        ParkingGuard();
                         break;
                     case '3':
-                        ;
+                        ParkingOwner();
                         break;
                 }
             }
+        }
+
+        public static void ParkingGuest()
+        {
+            {
+                Console.WriteLine("Välkommen Parkeringsgäst!");
+                Console.WriteLine("Vad har du för fordonstyp");
+                Console.WriteLine("Tryck 1 för bil");
+                Console.WriteLine("Tryck 2 för motorcykel");
+                Console.WriteLine("Tryck 3 för buss");
+                Console.WriteLine("Tryck 0 för att gå tillbaka till förra menyn");
+
+
+                ConsoleKeyInfo key = Console.ReadKey();
+                Console.Clear();
+
+                switch (key.KeyChar)
+                {
+                    case '1':
+                        Console.WriteLine("Du har en bil");
+                        break;
+                    case '2':
+                        Console.WriteLine("Du har en motorcykel");
+                        break;
+                    case '3':
+                        Console.WriteLine("Du har en buss");
+                        break;
+                    case '0':
+                        CheckIn();
+                        break;
+                }
+                
+                        Console.WriteLine("Skriv in registreringsnummer:");
+                        string regNr = Console.ReadLine();
+                        Console.WriteLine("Ditt registreringsnummer är: " + regNr);
+
+                        Console.WriteLine("Skriv in bilmärke: ");
+                        string carBrand = Console.ReadLine();
+                        Console.WriteLine("Ditt bilmärke är: " + carBrand);
+                        Console.WriteLine("Skriv in färg: ");
+                        string color = Console.ReadLine();
+                        Console.WriteLine("Din färg är: " + color);
+                        Console.WriteLine("Hur länge vill du parkera? ");
+                        string parkTime = Console.ReadLine();
+                        Console.WriteLine("Du vill parkera i " + parkTime + " minuter");
+                        Console.WriteLine("Ditt pris blir: " + parkTime + "kr");
+                   
+            }
+        }
+
+            public static void ParkingGuard()
+        {
+            Console.WriteLine("Välkommen Jan-Erik");
+        }
+
+        public static void ParkingOwner()
+        {
+            Console.WriteLine("Välkommen Karen!");
         }
     }
 }
