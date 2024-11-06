9b646fe3914ac7d5e626deb5b34e9f7b61d46f73
Alexander Åberg <alexander_aberg1985@msn.com>
2024-11-06 11:09:19 +0100

    Created helper class and put the car in it

---
+++ Parkeringsgarage/Helpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parkeringsgarage
+{
+    public class Helpers
+    {
+        public string ElBil { get; set; }
+        public Helpers(string elBil)
+        {
+            ElBil = elBil;
+        }
+    }
+}
