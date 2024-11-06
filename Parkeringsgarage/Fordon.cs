e869dc935b24367dcc4c144c928ef541beb0ef99
Alexander Åberg <alexander_aberg1985@msn.com>
2024-11-06 11:09:41 +0100

    Created class Fordon and created car in it

---
+++ Parkeringsgarage/Fordon.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Parkeringsgarage
+{
+    public class Fordon
+    {
+        public string RegNr { get; set; }
+        public int Row { get; set; }
+        public int Col { get; set; }
+
+        public Fordon(string regNr)
+        {
+            RegNr = regNr;
+        }
+    }
+
+    // The thief
+    public class Car : Fordon
+    { 
+        
+        public List<Helpers> ElBil { get; set; } = new List<Helpers>();
+        public Car(string regNr, List<Helpers> elBil) : base(regNr)
+        {
+            ElBil = elBil;
+        }
+        public override string ToString() => $"Car {RegNr}";
+
+    }
+}
